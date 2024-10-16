2f06a21f60b52f11362ee9682da16472e18556bb
vboxuser <vboxuser@IoT.myguest.virtualbox.org>
2024-10-16 16:34:44 +0200

    fix(part2): I modify my vagrantfile to deploy the app inside the vagrant machine and to run it automatically and i add the url in the /etc/hosts to be abble to access to the web site for the part 3

--- part3/installation.sh
+++ part3/installation.sh
@@ -5,19 +5,28 @@ sudo apt install -y docker.io
 sudo usermod -aG docker $USER
 
 # Install K3D
+echo "========== INSTALL K3D ==========="
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-# Create a K3D cluster
-k3d cluster create mycluster
+Create a K3D cluster
+# sudo k3d cluster create mycluster
+sudo k3d cluster create mycluster --port "30001:30001@server:0"
 
+
+echo "========== INSTALL kubectl ==========="
 # Install kubectl if necessary
-sudo apt install -y kubectl
+sudo apt update
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
 
 # Install Argo CD
-kubectl create namespace argocd
-kubectl create namespace dev
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo "========== INSTALL argocd ==========="
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "Installation complete. Use 'kubectl port-forward svc/argocd-server -n argocd 8080:443' to access the Argo CD UI."
+echo "########### Installation complete. Use 'kubectl port-forward svc/argocd-server -n argocd 8080:443' to access the Argo CD UI. ############"
 
-echo "to get the password use: kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="'{.data.password}'" | base64 --decode."
+echo "########### to get the password use: kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="'{.data.password}'" | base64 --decode ###########"
